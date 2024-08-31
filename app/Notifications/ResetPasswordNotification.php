723e66c7e9471e1d17007a2c0e7c1738db770953
ENDO TAKUMI <endo.takumi.info@gmail.com>
2024-08-31 17:34:58 +0900

    fix: メールを修正

---
+++ app/Notifications/ResetPasswordNotification.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ResetPasswordNotification extends Notification
+{
+    use Queueable;
+
+    private string $token;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(string $token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(mixed $notifiable): MailMessage
+    {
+        $frontendUrl = config('app.frontend_url', 'http://localhost:3000');
+        $url = $frontendUrl . '/auth/reset-password/' . $this->token . '?email=' . urlencode($notifiable->email);
+
+        return (new MailMessage())
+            ->subject('【' . config('app.name') . '】パスワード再設定')
+            ->markdown('emails.passwordreset', ['reset_url' => $url]);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
